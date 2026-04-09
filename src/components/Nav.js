e938304d7d41a1501315b6897003dc6c013a19de
huyboon <huyht0704@gmail.com>
2026-04-09 09:12:05 +0700

    establish meta tags and Open Graph protocol data

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Menu</a>
+                </li>
+                <li>
+                    <a href="#">Booking</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
